ed804a852619c6df73f16f9a82b27d73b622381a
FabeaRahman <fabearahman@gmail.com>
2025-07-08 13:05:22 +0600

    Add files via upload

---
+++ row 5 column 4 star print.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,a;
+    ///scanf ("%d",&a);
+    for(i=1;i<=4;i++){
+        for(j=0;j<=5;j++){
+            //printf("%d",i);
+printf("* ");
+        }
+        printf("\n");
+/* * * * * * *
+   * * * * * *
+   * * * * * *
+   * * * * * * */
+    }
+    return 0;
+}
