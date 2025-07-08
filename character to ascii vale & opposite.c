5c4f43a0cbf2e95788f86bc52425119215f3104d
FabeaRahman <fabearahman@gmail.com>
2025-07-08 12:39:07 +0600

    Add files via upload

---
+++ character to ascii vale & opposite.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main(){
+/// character --> ascii value
+char ch;
+printf(" Enter a ascii characters: ");
+scanf("%c", &ch);
+printf(" corresponding value : %d\n\n", ch);
+///ascii value --> character
+int a;
+printf(" Enter a value: ");
+scanf("%d", &a);
+printf(" corresponding ascii character : %c\n", a);
+return 0;
+}
