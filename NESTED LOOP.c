0243d935c7271a9bb160367661488ac9fb5388d9
FabeaRahman <fabearahman@gmail.com>
2025-07-08 13:01:24 +0600

    Add files via upload

---
+++ NESTED LOOP.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,a;
+    scanf ("%d",&a);
+    for(i=1;i<=a;i++){
+        for(j=1;j<=i;j++){
+            //printf("%d",i);
+printf("*");
+        }
+        printf("\n");
+//5
+/*
+**
+***
+****
+*****/
+    }
+    return 0;
+}
