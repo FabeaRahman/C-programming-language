b367d7e830461d917098002cac59f41477337bd8
FabeaRahman <fabearahman@gmail.com>
2025-07-08 13:13:15 +0600

    Add files via upload

---
+++ age of a country.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() {
+    int currentYear, age;
+
+    // Taking input
+    scanf("%d", &currentYear);
+
+    // Calculating age
+    age = currentYear - 1971;
+
+    // Output the result
+    printf("%d\n", age);
+
+    return 0;
+}
