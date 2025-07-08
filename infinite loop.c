d39c50d3b952ee90e04c8e1062208225745d2f4c
FabeaRahman <fabearahman@gmail.com>
2025-07-08 12:54:58 +0600

    Update infinite loop.c

--- infinite loop.c
+++ infinite loop.c
@@ -2,10 +2,30 @@
 // Driver code
 int main(){
 int i,n,sum=0;
-for (i = 1; ; i++){
+for (i = 1; ; i++){///for( ; ; )
 sum=sum+i;
 printf( "%d\n",sum);
 }
 ///printf( "Hello World\n");
 return 0;
+}
+#include<stdio.h>
+///while loop
+int main(){
+while(1)
+    {
+        printf("Fabea\n");
+    }
+    return 0;
+}
+#include<stdio.h>
+///do while loop
+int main(){
+    do
+
+    {
+        printf("Fabea\n");
+    }
+    while(1);
+    return 0;
 }
