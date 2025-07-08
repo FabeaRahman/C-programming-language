59cbe01bf948e1900d89b96f510041c985fcd611
FabeaRahman <fabearahman@gmail.com>
2025-07-08 12:46:47 +0600

    Add files via upload

---
+++ double divide.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int a=5,b=2;
+double divide =(double)a/b;
+printf("%lf",divide);
+return 0;
+}/**integer to float**/
+#include<stdio.h>
+int main(){
+double a=5.5,b=2.55;
+long long divide =(double)a/b;
+printf("%lld",divide);
+return 0;
+}
+/** float to integer**/
