5e0b7315d89942d4aaddda2158019bb8f7b7831d
FabeaRahman <fabearahman@gmail.com>
2025-07-08 13:07:33 +0600

    Add files via upload

---
+++ sum of ascci code.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+
+int main()
+{
+   char num1 ='9';
+   char num2 ='7';
+   int sum=(num1-48)+(num2-48);
+   printf("%d",sum);/// for %d 16 will be the ans if %c we use thr char for 16 will print
+   ///printf("%c",sum);
+   return 0;
+}
