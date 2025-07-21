06dfdc169cfe2b55cb6ff128370b4db4f31152c8
FabeaRahman <fabearahman@gmail.com>
2025-07-21 22:05:44 +0600

    Update reverse an array.c

--- reverse an array.c
+++ reverse an array.c
@@ -2,12 +2,15 @@
 int main ()
 {
     int i,n;
+    /// takes input size
     scanf("%d" ,&n);
     int ar [n];
+    /// array input loop
     for (i=0;i<n;i++){
         scanf("%d ",&ar[i]);
 
     }
+    /// print loop for print array
     for(i=n-1;i>=0;i--){
         printf("%d ",ar[i]);
     }
