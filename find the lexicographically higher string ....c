44a74362e3b2f0e8cd5e2ff8dd4de739bba5eb66
FabeaRahman <fabearahman@gmail.com>
2025-07-26 00:06:02 +0600

    Add files via upload

---
+++ find the lexicographically higher string ....c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char s1[100], s2[100];
+    scanf("%s %s", s1, s2);
+    if(strcmp(s1, s2) > 0)
+        printf("%s\n", s1);
+    else
+        printf("%s\n", s2);
+    return 0;
+}
